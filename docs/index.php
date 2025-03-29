ccd2f4b06727b5aa449dfb1bed7404566a181336
AbdullahAslan63 <abdullahaslan061212@gmail.com>
2025-03-29 23:30:09 +0300

    resimler terkar yüklendi

---
+++ docs/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarz Halı Concept Kataloğu</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">Tarz Halı Concept Katalog Resimlerinin Depolanması Amjacıyla Açılmış Repositry</h1>
+    <?php
+
+for ($i = 1; $i <= 40; $i++) {
+    echo '<img src="' . $i . '.webp" alt="Sayfa ' . $i . '">';
+
+    ?>
+</body>
+
+</html>
